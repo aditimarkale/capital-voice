a7c1c4fa475ec4746a0efdf045b2b56b1243c0b6
Aditi Markale <aditimarkale1@gmail.com>
2017-12-06 00:06:47 -0500

    Second Commit

--- message.php
+++ message.php
@@ -5,17 +5,17 @@ $url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country
 $file = file_get_contents($url);
 $data = json_decode($file, true);
 
-    foreach ($data as $character) {  
-        if($character['country'] == $body) {
-          $city = $character['city'];        
+   // foreach ($data as $character) {  
+     //   if($character['country'] == $body) {
+       //   $city = $character['city'];        
             echo "<Response>
     <Say voice="woman">
-           Capital of " . $body . " is " . $city . "
+           Capital 
     </Say>
 </Response>
-";
-            break;
-        }
-    }
+"//;
+   //         break;
+        //}
+    //}
 header('Content-Type: text/xml');
 ?>
