9b8284c6923d196ad86aaa2714950cd1d7ab18ca
Aditi Markale <aditimarkale1@gmail.com>
2017-12-07 20:10:31 -0500

    Fourth Commit

--- call.php
+++ call.php
@@ -1,21 +1,11 @@
 <?php
 $number = $_POST['From'];
-$body = $_POST['Body'];
-$url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
-$file = file_get_contents($url);
-$data = json_decode($file, true);
-
-    foreach ($data as $character) {  
-        if($character['country'] == $body) {
-          $city = $character['city'];        
+$body = $_POST['Body'];       
             echo "<Response>
     <Say>
-           Capital of " . $body . " is " . $city . "
+           Capital 
     </Say>
 </Response>
 ";
-            break;
-        }
-    }
 header('Content-Type: text/xml');
 ?>
