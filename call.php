f040791e684bea952428ed59be357707add733e0
Aditi Markale <aditimarkale1@gmail.com>
2017-12-10 18:13:54 -0500

    Sixth commit

--- call.php
+++ call.php
@@ -1,18 +1,16 @@
 <?php
+require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
+use Twilio\Rest\Client;
+$account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407'; 
+$auth_token = '6a7fed7f68a9d89d75996bc165982d2b'; 
+$client = new Client($account_sid, $auth_token); 
+
+
 $number = $_POST['From'];
-$body = $_POST['Body'];
-$url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
-$file = file_get_contents($url);
-$data = json_decode($file, true);
-
-
-
-    foreach ($data as $character) {  
-        if($character['country'] == $body) {
-          $city = $character['city'];         
-            break;
-        }
-    }
+//$body = $_POST['Body'];
+//$url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
+//$file = file_get_contents($url);
+//$data = json_decode($file, true);
 
 try {
         // Initiate a new outbound call
