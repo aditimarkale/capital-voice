fa7d08a5147ae6efea5c7f4f0fed12cf2c1a2bf7
Aditi Markale <aditimarkale1@gmail.com>
2017-12-10 18:24:01 -0500

    Commit 7

--- message.php
+++ message.php
@@ -5,7 +5,6 @@ $account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407';
 $auth_token = '6a7fed7f68a9d89d75996bc165982d2b'; 
 $client = new Client($account_sid, $auth_token); 
 
-
 $number = $_POST['From'];
 //$body = $_POST['Body'];
 //$url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
