40776b94897d1aa11698421802dac19c2e4703fb
Aditi Markale <aditimarkale1@gmail.com>
2017-12-11 21:47:47 -0500

    Commit 18

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ foreach ($data as $character) {
             "+12674407881",
             // Step 6: Set the URL Twilio will request when the call is answered.
            //array("url" => "http://demo.twilio.com/welcome/voice/")
-           array("url" => "https://raw.githubusercontent.com/aditimarkale/capital-voice/master/voice.xml")   
+           array("url" => "https://www.dropbox.com/s/mdx9qr94kpxhehm/voice.xml")   
         );
         //echo "Started call: " . $call->sid;
         /*echo "<Response>
