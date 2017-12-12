c196cb94f52f118385f0bc15e4d5babc783b96fc
Aditi Markale <aditimarkale1@gmail.com>
2017-12-11 22:30:58 -0500

    Commit 20

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-  require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
+    require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
     use Twilio\Rest\Client;
     $account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407'; 
     $auth_token = '6a7fed7f68a9d89d75996bc165982d2b'; 
     $client = new Client($account_sid, $auth_token); 
 
-$number = $_POST['From'];
+/*$number = $_POST['From'];
 $body = $_POST['Body'];
 $url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
 $file = file_get_contents($url);
-$data = json_decode($file, true);
+$data = json_decode($file, true);*/
 
-foreach ($data as $character) {  
+/*foreach ($data as $character) {  
         if($character['country'] == $body) {
           $city = $character['city'];
             break;
         }
-}
+}*/
             try {
         // Initiate a new outbound call
         $call = $client->account->calls->create(
@@ -24,15 +24,10 @@ foreach ($data as $character) {
             "+12674407881",
             // Step 6: Set the URL Twilio will request when the call is answered.
            //array("url" => "http://demo.twilio.com/welcome/voice/")
-           array("url" => "https://troubled-gun-1513.twil.io/assets/index.php")   
+           array("url" => "https://troubled-gun-1513.twil.io/assets/voice.php")   
         );
         //echo "Started call: " . $call->sid;
-        echo "<Response>
-                <Say>
-                    Capital of " . $body . " is " . $city . "
-                </Say>  
-                </Response>" . $call->sid;
-                
+        
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage();
     }
---
+++ voice.php
@@ -0,0 +1,26 @@
+<!--<Response>
+    <Say voice="alice">Capital
+    </Say>
+</Response>-->
+
+<?php
+$number = $_POST['From'];
+$body = $_POST['Body'];
+$url = 'https://raw.githubusercontent.com/samayo/country-json/master/src/country-by-capital-city.json';
+$file = file_get_contents($url);
+$data = json_decode($file, true);
+
+foreach ($data as $character) {  
+        if($character['country'] == $body) {
+          $city = $character['city'];
+            echo "<Response>
+    <Say>
+           Capital of " . $body . " is " . $city . "
+    </Say>
+</Response>
+";
+            break;
+        }
+}
+header('Content-Type: text/xml');
+?>
