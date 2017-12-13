0a3bb3324ed9cd301e03dbeb460a19c5b2edf1fc
Aditi Markale <aditimarkale1@gmail.com>
2017-12-12 23:13:54 -0500

    Commit 30

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
+    require 'twilio-php-master/Services/Twilio.php';
     use Twilio\Rest\Client;
     $account_sid = 'ACc2ee9a0b7bd01ddfd3079fc23a433407'; 
     $auth_token = '6a7fed7f68a9d89d75996bc165982d2b'; 
@@ -19,11 +20,6 @@ foreach ($data as $character) {
             break;
         }
 }
-/*$xml = new DOMDocument();                                  
-$xml_city = $xml->createElement("Say",'city');            
-$xml->appendChild($xml_city);                        
-$xml->save("voice.xml"); */
-
             try {
         // Initiate a new outbound call
         $call = $client->account->calls->create(
@@ -31,11 +27,9 @@ $xml->save("voice.xml"); */
             "+12674407881",
             // Step 6: Set the URL Twilio will request when the call is answered.
            //array("url" => "http://demo.twilio.com/welcome/voice/")
-           //array("url" => "https://raw.githubusercontent.com/aditimarkale/capital-voice/master/voice.php")   
-            array("url" => "https://aditivoice.herokuapp.com/")   
+           array("url" => "https://raw.githubusercontent.com/aditimarkale/capital-voice/master/voice.php")   
         );
-   
-        
+                
     } catch (Exception $e) {
         echo "Error: " . $e->getMessage();
     }
@@ -46,7 +40,5 @@ $xml->save("voice.xml"); */
         $response->say($city, ['voice' => 'alice']);
         echo $response;
 */
-
-
 header('Content-Type: text/xml');
 ?>
--- voice.php
+++ voice.php
@@ -1,20 +1,9 @@
 <?php
-/*require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
-//require_once '/index.php';
-use Twilio\Twiml;
-
-$twiml = new Services_Twilio_Twiml();
-$twiml->say( 'Hello Mark');
-//echo $response;*/
-?>
-
-
-<?php
 require_once 'twilio-php-master/Twilio/autoload.php';
+require 'twilio-php-master/Services/Twilio.php';
 use Twilio\Twiml;
 
-$response = new Twiml();
-$response->say('Chapeau!', ['voice' => 'woman', 'language' => 'fr']);
-
-echo $response;
+$response = new Services_Twilio_Twiml();
+$response->say('Hello');
+print $response;
 ?>
