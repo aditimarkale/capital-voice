2bdf3f360b2e17d95b55cd0dc28baaf6868496ef
Aditi Markale <aditimarkale1@gmail.com>
2017-12-13 18:52:29 -0500

    Commit 35

--- index.php
+++ index.php
@@ -35,12 +35,5 @@ foreach ($data as $character) {
     }
 
 header('Content-Type: text/xml');
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+//echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
-
-<?xml version="1.0" encoding= "UTF-8" ?>
-<Response>
-    <Say>
-        <?php echo $city; ?>
-    </Say>
-</Response>
