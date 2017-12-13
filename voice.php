e784404c818aac3319c7d5a89440eadbe2d7c75b
Aditi Markale <aditimarkale1@gmail.com>
2017-12-12 22:40:41 -0500

    Commit 28

--- voice.php
+++ voice.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'twilio-php-master/Twilio/autoload.php'; // Loads the library
-require_once '/index.php';
+//require_once '/index.php';
 use Twilio\Twiml;
 
-$response = new Twiml();
-$response->say($city, ['voice' => 'woman']);
-echo $response;
+$twiml = new Services_Twilio_Twiml();
+$twiml->say( 'Hello Mark');
+//echo $response;
 ?>
