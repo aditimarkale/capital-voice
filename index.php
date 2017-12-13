ec02cc81c57cca8048435a135e30d25c730288e4
Aditi Markale <aditimarkale1@gmail.com>
2017-12-13 18:50:28 -0500

    Commit 34

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ foreach ($data as $character) {
             "+12674407881",
             // Step 6: Set the URL Twilio will request when the call is answered.
            //array("url" => "http://demo.twilio.com/welcome/voice/")
-           array("url" => "https://troubled-gun-1513.twil.io/assets/index.php")   
+           array("url" => "https://troubled-gun-1513.twil.io/assets/voice.php")   
         );
                 
     } catch (Exception $e) {
